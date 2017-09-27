bdb8c3973b8f86f347b916e3c63d2d2fc0b4a296
Alexey.Bedonik <alexey.bedonik@kandasoft.com>
2017-09-27 14:52:27 +0300

    bug with updating the pendo statistic when opening child routes is fixed.

--- addon/initializers/router.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export function initialize() {
-  Ember.Router.reopen({
-    pendoPageLoad: function () {
-      window.pendo.pageLoad();
-    }.on('didTransition')
-  });
-}
---
+++ app/initializers/pendo.js
@@ -0,0 +1,1 @@
+export {default, initialize} from 'ember-cli-pendo-utils/initializers/pendo';
