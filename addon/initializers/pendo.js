313687a67e4308c40a28cd1b078c3275950b4ebd
Alexey.Bedonik <alexey.bedonik@kandasoft.com>
2017-10-04 19:39:26 +0300

    Adds check to pendo exist and increase version.

--- addon/initializers/pendo.js
+++ addon/initializers/pendo.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export function initialize() {
     Ember.Router.reopen({
         pendoPageLoad: Ember.on('didTransition', function () {
-            if (window.pendo !== undefined) {
-              window.pendo.pageLoad();
+            if (window.pendo) {
+                window.pendo.pageLoad();
             }
         })
     });
